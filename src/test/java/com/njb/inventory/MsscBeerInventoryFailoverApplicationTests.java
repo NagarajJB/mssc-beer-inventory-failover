9f2c789c0ba13428c5225318b1baa99eefac6a82
Nagaraja Balakrishna <nbalakrishna@manh.com>
2020-06-04 22:32:33 +0530

    initial commit

---
+++ src/test/java/com/njb/inventory/MsscBeerInventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.njb.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerInventoryFailoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
